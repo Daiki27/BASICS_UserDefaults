efde99e35f770500cac3d3aba95e878dae57b757
DaikiHiguchi <daikiit27@gmail.com>
2021-06-08 01:25:25 +0900

    initial commit

---
+++ BASICS_UserDefaultsTests/BASICS_UserDefaultsTests.swift
@@ -0,0 +1,33 @@
+//
+//  BASICS_UserDefaultsTests.swift
+//  BASICS_UserDefaultsTests
+//
+//  Created by 樋口大樹 on 2021/06/07.
+//
+
+import XCTest
+@testable import BASICS_UserDefaults
+
+class BASICS_UserDefaultsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
